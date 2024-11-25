b6f6c70059f9273868b49c541b267ef84820d3af
Alexandra Augur <alexandraaugur@Alexandras-MacBook-Air.local>
2024-11-25 09:24:06 -0500

    dependency fix

--- api/config.py
+++ api/config.py
@@ -7,8 +7,6 @@ load_dotenv()
 class TestConfig:
     TESTING = True
     SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
-    # 'postgresql://teach_u_database_user:rY8C1keKpUZ0lHPHgL3gSflxAMDu5TBy@dpg-cnfhraol5elc73948sg0-a.oregon-postgres.render.com/teach_u_database'
-    #SQLALCHEMY_DATABASE_URI ='sqlite:///:memory:'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('FLASK_SECRET_KEY')
     FLASK_SQLALCHEMY_ECHO=True
